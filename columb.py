4aff3b3eb3de40a47005cb47c940ac7e66081074
Faruk Baktas <70661808+farukbaktas@users.noreply.github.com>
2024-01-06 09:53:25 +0300

    Last Push

---
+++ columb.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from io import StringIO
+import matplotlib.pyplot as plt
+
+# Paste your dataset here or specify a file path
+data = """
+age,workclass,fnlwgt,education,education-num,marital-status,occupation,relationship,race,sex,capital-gain,capital-loss,hours-per-week,native-country,class
+39,State-gov,77516,Bachelors,13,Married-civ-spouse,Adm-clerical,Wife,White,Female,2174,0,40,United-States,<=50K
+50,Self-emp-not-inc,83311,Bachelors,13,Divorced,Exec-managerial,Not-in-family,White,Male,0,0,13,United-States,<=50K
+38,Private,215646,HS-grad,9,Married-civ-spouse,Handlers-cleaners,Wife,White,Female,0,0,40,United-States,<=50K
+53,Private,234721,11th,7,Divorced,Handlers-cleaners,Not-in-family,Black,Female,0,0,40,United-States,<=50K
+28,Private,338409,Bachelors,13,Divorced,Prof-specialty,Unmarried,Asian-Pac-Islander,Male,0,0,40,Cambodia,<=50K
+"""
+
+# Reading data as a file using StringIO
+df = pd.read_csv(StringIO(data))
+
+# Drawing column chart based on "race" property
+race_counts = df['race'].value_counts()
+race_counts.plot(kind='bar', color='skyblue', edgecolor='black')
+plt.title('Distribution of ras')
+plt.xlabel('Race')
+plt.ylabel('Frequency')
+plt.show()
---
+++ dot.py
@@ -0,0 +1,22 @@
+import pandas as pd
+from io import StringIO
+import matplotlib.pyplot as plt
+
+data = """
+age,workclass,fnlwgt,education,education-num,marital-status,occupation,relationship,race,sex,capital-gain,capital-loss,hours-per-week,native-country,class
+39,State-gov,77516,Bachelors,13,Married-civ-spouse,Adm-clerical,Wife,White,Female,2174,0,40,United-States,<=50K
+50,Self-emp-not-inc,83311,Bachelors,13,Divorced,Exec-managerial,Not-in-family,White,Male,0,0,13,United-States,<=50K
+38,Private,215646,HS-grad,9,Married-civ-spouse,Handlers-cleaners,Wife,White,Female,0,0,40,United-States,<=50K
+53,Private,234721,11th,7,Divorced,Handlers-cleaners,Not-in-family,Black,Female,0,0,40,United-States,<=50K
+28,Private,338409,Bachelors,13,Divorced,Prof-specialty,Unmarried,Asian-Pac-Islander,Male,0,0,40,Cambodia,<=50K
+"""
+
+# Reading data as a file using StringIO
+df = pd.read_csv(StringIO(data))
+
+# Drawing dot plot based on "age" property
+plt.scatter(range(len(df)), df['age'], color='green', marker='o')
+plt.title('Dot Plot by Age')
+plt.xlabel('Index')
+plt.ylabel('Age')
+plt.show()
